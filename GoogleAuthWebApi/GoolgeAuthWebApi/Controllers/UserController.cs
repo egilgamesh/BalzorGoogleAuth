8b5383c7bf889da8acd42b3f03ff14ddcc536266
egilgamesh <egilgamesh@outlook.com>
2022-01-31 22:27:43 +0100

    Minor fix for solution structure

---
+++ GoogleAuthWebApi/GoolgeAuthWebApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoolgeAuthWebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private IConfiguration? configuration;
+
+		[HttpGet("GoogleSignIn")]
+		public async Task GoogleSignIn()
+		{
+			configuration = new ConfigurationManager();
+			await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, 
+				new AuthenticationProperties { RedirectUri = configuration.GetSection("Google:RedirectUri").Value});
+		}
+	}
+
+}
