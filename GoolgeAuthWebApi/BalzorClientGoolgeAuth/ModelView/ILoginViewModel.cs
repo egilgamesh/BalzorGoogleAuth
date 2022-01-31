74436a299f1db5f9f952753340150d70be13fe67
unknown <egilgamesh@outlook.com>
2022-01-31 16:47:08 +0100

    Remove unused projects

--- GoolgeAuthWebApi/BalzorClientGoolgeAuth/ModelView/ILoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BalzorClientGoolgeAuth.ModelView;
-
-public interface ILoginViewModel
-{
-	public string EmailAddress { get; set; }
-	public string Password { get; set; }
-
-	public Task LoginUser();
-}
